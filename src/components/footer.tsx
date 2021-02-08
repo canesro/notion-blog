7ef7c847cd21b7a1aebd6ea9db573951ca20cb70
canesro <andytorock@gmail.com>
2021-02-08 22:05:33 +0900

    Change footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,21 +3,14 @@ import ExtLink from './ext-link'
 export default () => (
   <>
     <footer>
-      <span>Deploy your own!</span>
-      <ExtLink href="https://vercel.com/import/git?s=https://github.com/ijjk/notion-blog/tree/master&env=NOTION_TOKEN,BLOG_INDEX_ID&envDescription=Required+env+values+for+deploying&envLink=https://github.com/ijjk/notion-blog%23getting-blog-index-and-token">
-        <img
-          src="https://vercel.com/button"
-          height={46}
-          width={132}
-          alt="deploy to Vercel button"
-        />
-      </ExtLink>
-      <span>
-        or{' '}
+      <span>&copy; canesro.dev</span>
+      <br></br>
+      <small>
+        Powered by{' '}
         <ExtLink href="https://github.com/ijjk/notion-blog">
-          view source
+          Notion Blog
         </ExtLink>
-      </span>
+      </small>
     </footer>
   </>
 )
